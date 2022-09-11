5cdac476e32729f77b3e76336ef92221e712ba98
您的姓名 <105603565+MasterYan01@users.noreply.github.com>
2022-09-11 20:47:07 +0800

    first commit

---
+++ app/assets/js/all.js
@@ -0,0 +1,16 @@
+const swiper = new Swiper('.swiper', {
+  // 分頁   
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  // 左右箭頭    
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  // 滾動條
+  scrollbar: {
+    el: '.swiper-scrollbar',
+    draggable: true,
+  },
+});
